fc2251f6f488cad266bdfd6e9319d62d3180178a
Mihai laptop <mihaiionstefan@gmail.com>
2020-07-18 17:48:15 +0300

    added Product Entity

---
+++ src/Entity/Product.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity ;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+/**
+ * Product
+ * @ORM\Table(name="Product")
+ * @ORM\Entity
+ */
+class Product {
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="Id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Seller", type="string", nullable=false)
+     * @Assert\NotBlank
+     * @Assert\Length(min=10)
+     */
+    private $seller ;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Name", type="string", nullable=false)
+     * @Assert\NotBlank
+     * @Assert\Length(min=10)
+     */
+    private $name ;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Seller", type="string", nullable=true)
+     */
+    private $description ;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Image", type="string", nullable=true)
+     */
+    private $image ;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="Price", type="integer", nullable=false)
+     * @Assert\NotBlank
+     */
+    private $price ;
+
+
+}
